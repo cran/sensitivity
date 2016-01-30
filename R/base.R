502ef803f169ad7e8007a720121ef583c0980213
Frank Weber <frank.weber@outlook.de>
2016-01-30 21:56:16 +0100

    Adapted behaviour for "loop = TRUE" in response() for use with model functions returning a matrix or a list of matrices of the same dimensions.

--- R/base.R
+++ R/base.R
@@ -17,8 +17,53 @@ response <- function(x, loop = FALSE, ...) {
     if (loop) {
       n <- nrow(x$X)
       y <- numeric(n)
-      for (i in 1:n) {
-        y[i] <- x$model(x$X[i,], ...)
+      y <- sapply(1:n, function(i){
+        x$model(x$X[i,], ...)
+      })
+      if(is.matrix(y)){
+        # This means the model function returned vectors, so y is a 
+        # (m times n)-matrix (with unknown m). For better consistency with the 
+        # default case that y is a vector of length n, y is transposed to a 
+        # (n times m)-matrix.
+        # (Remark: Per default, sapply creates one column for every element the 
+        # function is applied to.)
+        y <- t(y)
+      } else if(is.list(y)){
+        check_matrices <- vapply(y, function(x) is.matrix(x), 
+                                 FUN.VALUE = logical(1))
+        if(all(check_matrices)){
+          dims <- vapply(y, function(x) dim(x), FUN.VALUE = numeric(2))
+          check_dims <- apply(dims, 1, function(x) length(unique(x)) == 1)
+          if(all(check_dim)){
+            # This means the model function returned matrices of the same sizes.
+            # Let m be the number of rows and z be the number of columns.
+            # For better consistency, results are transposed so that y is a list
+            # of length z containing in each element a (n times m)-matrix.
+            
+            # Number of rows and columns of all matrices:
+            dims <- dims[, 1]
+            # Save the column names for restoring them later:
+            y_colnames <- colnames(y[[1]])
+            # y_mat is a ((n * z) times m)-matrix:
+            y_mat <- t(do.call(cbind, y))
+            # The rownames don't make sense anymore:
+            rownames(y_mat) <- NULL
+            nz <- nrow(y_mat)
+            y <- lapply(1:dims[2], function(j){
+              y_mat[seq(j, nz, dims[2]), , drop = FALSE]
+            })
+            # Restore the original column names (these are now the names of the
+            # elements of y):
+            names(y) <- y_colnames
+          } else{
+            stop("x$model returned a list of matrices, but they are not all",
+                 "of the same dimensions")
+          }
+        } else{
+          stop("x$model returned a list, but not all elements are matrices")
+        }
+      } else{
+        stop("x$model returned an object that can't be handled")
       }
     } else {
       y <- x$model(x$X, ...)
