0e496b92188af8d3e80997c3a3c0421198b264ef
Frank Weber <frank.weber@outlook.de>
2016-01-31 14:02:54 +0100

    Removed duplicated version of response() in "sobolowen.R".

--- R/sobolowen.R
+++ R/sobolowen.R
@@ -35,33 +35,6 @@ sobolowen <- function(model = NULL, X1, X2, X3, nboot = 0, conf = 0.95, varest =
   return(x)
 }
 
-response <- function(x, loop = FALSE, ...) {
-  id <- deparse(substitute(x))
-  if (class(x$model) == "function") {
-    if (loop) {
-      n <- nrow(x$X)
-      y <- numeric(n)
-      for (i in 1:n) {
-        y[i] <- x$model(x$X[i,], ...)
-      }
-    } else {
-      y <- x$model(x$X, ...)
-    }
-  } else if (TRUE %in% (paste("predict.", class(x$model), sep="") %in% methods(predict))) {
-    y <- predict(x$model, x$X, ...)
-  } else {
-    stop("The model isn't a function or does not have a predict method")
-  }
-  
-  if (class(y) != "numeric") {
-    y <- as.numeric(y)
-    warning("Conversion of the response to numeric")
-  }
-  
-  x$y <- y
-  assign(id, x, parent.frame())
-} 
-
 estim.sobolowen <- function(data, i=1:nrow(data), varest=2) {
   d <- as.matrix(data[i, ]) # as.matrix for colSums
   n <- nrow(d)
