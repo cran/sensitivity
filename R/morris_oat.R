e9ec17cde882f262fc81ea3e9a8554fc294d8d0c
Frank Weber <frank.weber@outlook.de>
2016-02-04 08:34:03 +0100

    Adapted ee.oat() for y being a matrix or an array.

--- R/morris_oat.R
+++ R/morris_oat.R
@@ -36,14 +36,57 @@ ee.oat <- function(X, y) {
 # compute the elementary effects for a OAT design
   p <- ncol(X)
   r <- nrow(X) / (p + 1)
-  comp_ee_i <- function(i){
-    j <- ind.rep(i, p)
-    j1 <- j[1 : p]
-    j2 <- j[2 : (p + 1)]
-    # return((y[j2] - y[j1]) / rowSums(X[j2,] - X[j1,]))
-    return(solve(X[j2,] - X[j1,], y[j2] - y[j1]))
+  if(class(y) == "numeric"){
+    one_i_vector <- function(i){
+      j <- ind.rep(i, p)
+      j1 <- j[1 : p]
+      j2 <- j[2 : (p + 1)]
+      # return((y[j2] - y[j1]) / rowSums(X[j2,] - X[j1,]))
+      return(solve(X[j2,] - X[j1,], y[j2] - y[j1]))
+    }
+    ee <- vapply(1:r, one_i_vector, FUN.VALUE = numeric(p))
+    ee <- t(ee)
+    # "ee" is now a (r times p)-matrix.
+  } else if(class(y) == "matrix"){
+    one_i_matrix <- function(i){
+      j <- ind.rep(i, p)
+      j1 <- j[1 : p]
+      j2 <- j[2 : (p + 1)]
+      return(solve(X[j2,] - X[j1,], 
+                   y[j2, , drop = FALSE] - y[j1, , drop = FALSE]))
+    }
+    ee <- vapply(1:r, one_i_matrix, 
+                 FUN.VALUE = matrix(0, nrow = p, ncol = ncol(y)))
+    # Transpose "ee" (an array of dimensions c(p, ncol(y), r)) to an array of
+    # dimensions c(r, p, ncol(y)) (for better consistency with the standard 
+    # case that "class(y) == "numeric""):
+    ee <- aperm(ee, perm = c(3, 1, 2))
+  } else if(class(y) == "array"){
+    one_i_array <- function(i){
+      j <- ind.rep(i, p)
+      j1 <- j[1 : p]
+      j2 <- j[2 : (p + 1)]
+      ee_per_3rd_dim <- sapply(1:(dim(y)[3]), function(idx_3rd_dim){
+        y_j2_matrix <- y[j2, , idx_3rd_dim]
+        y_j1_matrix <- y[j1, , idx_3rd_dim]
+        # Correction needed if "dim(y)[2] == 1", so "y_j2_matrix" and 
+        # "y_j1_matrix" have been dropped to vectors:
+        if(class(y_j2_matrix) == "numeric"){
+          y_j2_matrix <- matrix(y_j2_matrix)
+          y_j1_matrix <- matrix(y_j1_matrix)
+        }
+        # Here, the result of "solve(...)" is a (p times dim(y)[2])-matrix:
+        solve(X[j2,] - X[j1,], y_j2_matrix - y_j1_matrix)
+      }, simplify = "array")
+      # "ee_per_3rd_dim" is an array of dimensions c(p, dim(y)[2], dim(y)[3]).
+      # Assign the corresponding names for the third dimension:
+      dimnames(ee_per_3rd_dim)[[3]] <- dimnames(y)[[3]]
+      return(ee_per_3rd_dim)
+    }
+    ee <- sapply(1:r, one_i_array, simplify = "array")
+    # "ee" is an array of dimensions c(p, dim(y)[2], dim(y)[3], r), so it is
+    # transposed to an array of dimensions c(r, p, dim(y)[2], dim(y)[3]):
+    ee <- aperm(ee, perm = c(4, 1, 2, 3))
   }
-  ee <- vapply(1:r, comp_ee_i, FUN.VALUE = numeric(p))
-  ee <- t(ee)
   return(ee)
 }
